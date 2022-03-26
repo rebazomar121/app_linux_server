41c0ecd5b3cab8539e7611486e62137b5d12ef72
rebaz omar <58820960+rebazomar121@users.noreply.github.com>
2022-03-26 13:59:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const mongoose = require("mongodb");
 const dotenv = require("dotenv");
 const cookieParser = require('cookie-parser')
 const cors = require("cors");
@@ -11,16 +11,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
-
 app.use(express.json());
-
 // coockieParser use for change cookie to change cookie to js code 
 app.use(cookieParser())
 // this will just return data for one front end {we get it}
 app.use(cors({
-  origin:['http://localhost:3000'],
+  origin: ['http://localhost:3000'],
   // this will allow to website store cookie
-  credentials:true
+  credentials: true
 }));
 
 // connect to mongoDB
